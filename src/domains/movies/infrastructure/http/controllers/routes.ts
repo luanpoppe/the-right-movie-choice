df0eeb60587b740abcf1ccc1107abd0469a3ddb4
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-10-02 19:02:18 -0300

    feat: organizing docs for the swagger

--- src/domains/movies/infrastructure/http/controllers/routes.ts
+++ src/domains/movies/infrastructure/http/controllers/routes.ts
@@ -1,29 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { movieRecommendationController } from "./movie-recommendation.controller";
-import {
-  MovieRecommendationRequestDTOSchema,
-  MovieRecommendationResponseDTOSchema,
-} from "../dto/movie-recommendation.dto";
-import { HeadersDTOSchema } from "@/infrastructure/http/dto/headers.dto";
-import z from "zod";
-import { WrongMovieSchemaFromLlmException } from "@/domains/movies/domain/exceptions/wrong-movie-schema-from-llm.exception";
+import { MovieRecommendationDocs } from "../docs/movie-recommendation.docs";
 
 export function moviesControllers(app: FastifyInstance) {
   app.post(
     "/movie/recommendation",
-    {
-      schema: {
-        body: MovieRecommendationRequestDTOSchema,
-        headers: HeadersDTOSchema,
-        response: {
-          200: MovieRecommendationResponseDTOSchema,
-          400: z.object({ error: z.string() }),
-          500: z.object({
-            error: z.literal(new WrongMovieSchemaFromLlmException().message),
-          }),
-        },
-      },
-    },
+    MovieRecommendationDocs as any,
     movieRecommendationController
   );
 }
---
+++ src/domains/movies/infrastructure/http/docs/movie-recommendation.docs.ts
@@ -0,0 +1,26 @@
+import { HeadersDTOSchema } from "@/infrastructure/http/dto/headers.dto";
+import {
+  MovieRecommendationRequestDTOSchema,
+  MovieRecommendationResponseDTOSchema,
+} from "../dto/movie-recommendation.dto";
+import z from "zod";
+import { WrongMovieSchemaFromLlmException } from "@/domains/movies/domain/exceptions/wrong-movie-schema-from-llm.exception";
+import { RouteShorthandOptions } from "fastify";
+
+export const MovieRecommendationDocs: RouteShorthandOptions = {
+  schema: {
+    tags: ["movies"],
+    body: MovieRecommendationRequestDTOSchema,
+    headers: HeadersDTOSchema,
+    response: {
+      200: MovieRecommendationResponseDTOSchema.describe("Success"),
+      400: z.object({ error: z.string() }).describe("Bad Request"),
+      500: z.object({
+        error: z.enum([
+          new WrongMovieSchemaFromLlmException().message,
+          "Unkown Error",
+        ]),
+      }),
+    },
+  },
+};
