64ab47243abc49f002af16db3cc47a4680882fa2
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-10-03 14:37:05 -0300

    feat: improving documentation on swagger

--- src/domains/movies/infrastructure/http/docs/movie-recommendation.docs.ts
+++ src/domains/movies/infrastructure/http/docs/movie-recommendation.docs.ts
@@ -11,16 +11,19 @@ export const MovieRecommendationDocs: RouteShorthandOptions = {
   schema: {
     tags: ["movies"],
     body: MovieRecommendationRequestDTOSchema,
+    description: "Get movie recommendations based on the user message",
     headers: HeadersDTOSchema,
     response: {
       200: MovieRecommendationResponseDTOSchema.describe("Success"),
       400: z.object({ error: z.string() }).describe("Bad Request"),
-      500: z.object({
-        error: z.enum([
-          new WrongMovieSchemaFromLlmException().message,
-          "Unkown Error",
-        ]),
-      }),
+      500: z
+        .object({
+          error: z.enum([
+            new WrongMovieSchemaFromLlmException().message,
+            "Unkown Error",
+          ]),
+        })
+        .describe("Internal server error"),
     },
   },
 };
